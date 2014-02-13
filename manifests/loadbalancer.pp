a8742036c8d22b738cb53a003a059020435d42bf
Yanis Guenane <yanis.guenane@enovance.com>
2014-02-12 19:37:28 -0500

    Make loadbalancer.pp fit active/active or active/passive configuration
    This commit allows one to specify if the loadbalancer is deployed
    using haproxy/keepalived or haproxy/pacemaker, specifying the
    `$active_active` value. In one case HAProxy binds on `0.0.0.0`
    on the other it binds on the various vips (public, internal).
    
    Related #237

--- manifests/loadbalancer.pp
+++ manifests/loadbalancer.pp
@@ -32,11 +32,17 @@ class cloud::loadbalancer(
   $keystone_api                     = true,
   $horizon                          = true,
   $spice                            = true,
+  $lb_active_active                 = $os_params::lb_active_active,
+  $public_network                   = $os_params::public_network,
+  $internal_network                 = $os_params::internal_network,
+  $multicast_address                = $os_params::multicast_address,
   $haproxy_auth                     = $os_params::haproxy_auth,
   $keepalived_state                 = 'BACKUP',
   $keepalived_priority              = 50,
-  $keepalived_interface             = $os_params::keepalived_interface,
-  $keepalived_ipvs                  = [$os_params::vip_public_ip,$os_params::galera_ip],
+  $keepalived_public_interface      = $os_params::lb_public_netif,
+  $keepalived_public_ipvs           = [$os_params::vip_public_ip],
+  $keepalived_internal_interface    = $os_params::lb_internal_netif,
+  $keepalived_internal_ipvs         = [$os_params::vip_internal_ip],
   $keepalived_localhost_ip          = $os_params::keepalived_localhost_ip,
   $ks_cinder_public_port            = $os_params::ks_cinder_public_port,
   $ks_ceilometer_public_port        = $os_params::ks_ceilometer_public_port,
@@ -55,25 +61,113 @@ class cloud::loadbalancer(
   $horizon_port                     = $os_params::horizon_port,
   $spice_port                       = $os_params::spice_port,
   $vip_public_ip                    = $os_params::vip_public_ip,
+  $vip_internal_ip                  = $os_params::vip_internal_ip,
   $galera_ip                        = $os_params::galera_ip
 ){
 
-  class { 'haproxy': }
-
-  class { 'keepalived': }
-
-  keepalived::vrrp_script { 'haproxy':
-    name_is_process => true
+  if $keepalived_public_interface != $keepalived_internal_interface and $keepalived_public_ipvs != $keepalived_internal_ipvs {
+    $create_internal = true
+    $ensure_internal = present
+  } else {
+    $create_internal = false
+    $ensure_internal = absent
   }
 
-  keepalived::instance { '1':
-    interface     => $keepalived_interface,
-    virtual_ips   => unique(split(join(flatten([$keepalived_ipvs, ['']]), " dev ${keepalived_interface},"), ',')),
-    state         => $keepalived_state,
-    track_script  => ['haproxy'],
-    priority      => $keepalived_priority,
-    notify_master => '"/etc/init.d/haproxy start"',
-    notify_backup => '"/etc/init.d/haproxy stop"',
+  if $lb_active_active {
+
+    $lb_bind_ip = ['0.0.0.0']
+
+    class { 'haproxy': }
+
+    class { 'keepalived': }
+
+    keepalived::vrrp_script { 'haproxy':
+      name_is_process => true
+    }
+
+    keepalived::instance { '1':
+      interface     => $keepalived_public_interface,
+      virtual_ips   => unique(split(join(flatten([$keepalived_public_ipvs, ['']]), " dev ${keepalived_public_interface},"), ',')),
+      state         => $keepalived_state,
+      track_script  => ['haproxy'],
+      priority      => $keepalived_priority,
+    }
+
+    if $create_internal {
+      keepalived::instance { '2':
+        interface     => $keepalived_internal_interface,
+        virtual_ips   => unique(split(join(flatten([$keepalived_internal_ipvs, ['']]), " dev ${keepalived_internal_interface},"), ',')),
+        state         => $keepalived_state,
+        track_script  => ['haproxy'],
+        priority      => $keepalived_priority,
+      }
+    }
+
+  }
+  else {
+
+    $lb_bind_ip = unique([$vip_public_ip, $vip_internal_ip])
+    $a_public_network = split($public_network, '/')
+    $a_internal_network = split($internal_network, '/')
+
+    class { 'haproxy':
+      enable => false,
+    }
+
+    if !defined(Class['corosync']) {
+      class { 'corosync':
+        enable_secauth    => false,
+        authkey           => '/var/lib/puppet/ssl/certs/ca.pem',
+        bind_address      => $a_internal_network[0],
+        multicast_address => $multicast_address
+      }
+    }
+
+    ensure_resource('corosync::service', 'pacemaker', {'version' => '0'})
+    ensure_resource('cs_property', 'no-quorum-policy', {'value' => 'ignore'})
+    ensure_resource('cs_property', 'stonith-enabled', {'value' => 'false'})
+    ensure_resource('cs_property', 'pe-warn-series-max', {'value' => '1000'})
+    ensure_resource('cs_property', 'pe-input-series-max', {'value' => '1000'})
+    ensure_resource('cs_property', 'cluster-recheck-interval', {'value' => '5min'})
+
+    cs_primitive { 'p_public_vip':
+      primitive_class => 'ocf',
+      primitive_type  => 'IPaddr2',
+      provided_by     => 'heartbeat',
+      parameters      => { 'ip' => $vip_public_ip, 'cidr_netmask' => $a_public_network[1] },
+      operations      => { 'monitor' => { 'interval' => '1s' } },
+    }
+
+    cs_primitive { 'p_internal_vip':
+      ensure          => $ensure_internal,
+      primitive_class => 'ocf',
+      primitive_type  => 'IPaddr2',
+      provided_by     => 'heartbeat',
+      parameters      => { 'ip' => $vip_internal_ip, 'cidr_netmask' => $a_internal_network[1] },
+      operations      => { 'monitor' => { 'interval' => '1s' } },
+    }
+
+    file { '/usr/lib/ocf/resource.d/heartbeat/haproxy':
+      ensure => file,
+      source => 'puppet:///modules/cloud/heartbeat/haproxy',
+      owner  => 'root',
+      group  => 'root',
+      mode   => '0755';
+    }
+
+    cs_primitive { 'p_haproxy':
+      primitive_class => 'ocf',
+      primitive_type  => 'haproxy',
+      provided_by     => 'heartbeat',
+      parameters      => { 'conffile' => '/etc/haproxy/haproxy.cfg' },
+      operations      => { 'monitor'  => { 'interval' => '1s' } },
+      require         => [File['/usr/lib/ocf/resource.d/heartbeat/haproxy'], Cs_primitive['p_public_vip'], Cs_primitive['p_internal_vip']],
+    }
+
+    cs_group {'g_vip_with_haproxy':
+      primitives => [ 'p_public_vip', 'p_internal_vip', 'p_haproxy' ],
+    }
+
   }
 
   file { '/etc/logrotate.d/haproxy':
@@ -85,7 +179,7 @@ class cloud::loadbalancer(
   }
 
   haproxy::listen { 'monitor':
-    ipaddress => $vip_public_ip,
+    ipaddress => $lb_bind_ip,
     ports     => '9300',
     options   => {
       'mode'        => 'http',
@@ -99,10 +193,10 @@ class cloud::loadbalancer(
     cloud::loadbalancer::listen_http {
       'keystone_api_cluster':
         ports     => $ks_keystone_public_port,
-        listen_ip => $vip_public_ip;
+        listen_ip => $lb_bind_ip;
       'keystone_api_admin_cluster':
         ports     => $ks_keystone_admin_port,
-        listen_ip => $vip_public_ip;
+        listen_ip => $lb_bind_ip;
     }
   }
   if $swift_api {
@@ -110,35 +204,35 @@ class cloud::loadbalancer(
       'swift_api_cluster':
         ports     => $ks_swift_public_port,
         httpchk   => 'httpchk /healthcheck',
-        listen_ip => $vip_public_ip;
+        listen_ip => $lb_bind_ip;
     }
   }
   if $nova_api {
     cloud::loadbalancer::listen_http{
       'nova_api_cluster':
         ports     => $ks_nova_public_port,
-        listen_ip => $vip_public_ip;
+        listen_ip => $lb_bind_ip;
     }
   }
   if $ec2_api {
     cloud::loadbalancer::listen_http{
       'ec2_api_cluster':
         ports     => $ks_ec2_public_port,
-        listen_ip => $vip_public_ip;
+        listen_ip => $lb_bind_ip;
     }
   }
   if $metadata_api {
     cloud::loadbalancer::listen_http{
       'metadata_api_cluster':
         ports     => $ks_metadata_public_port,
-        listen_ip => $vip_public_ip;
+        listen_ip => $lb_bind_ip;
     }
   }
   if $spice {
     cloud::loadbalancer::listen_http{
       'spice_cluster':
         ports     => $spice_port,
-        listen_ip => $vip_public_ip,
+        listen_ip => $lb_bind_ip,
         httpchk   => 'httpchk GET /';
     }
   }
@@ -146,68 +240,68 @@ class cloud::loadbalancer(
     cloud::loadbalancer::listen_http{
       'glance_api_cluster':
         ports     => $ks_glance_api_public_port,
-        listen_ip => $vip_public_ip;
+        listen_ip => $lb_bind_ip;
     }
   }
   if $glance_registry {
     cloud::loadbalancer::listen_http{
       'glance_registry_cluster':
         ports     => $ks_glance_registry_internal_port,
-        listen_ip => $vip_public_ip;
+        listen_ip => $lb_bind_ip;
     }
   }
   if $neutron_api {
     cloud::loadbalancer::listen_http{
       'neutron_api_cluster':
         ports     => $ks_neutron_public_port,
-        listen_ip => $vip_public_ip;
+        listen_ip => $lb_bind_ip;
     }
   }
   if $cinder_api {
     cloud::loadbalancer::listen_http{
       'cinder_api_cluster':
         ports     => $ks_cinder_public_port,
-        listen_ip => $vip_public_ip;
+        listen_ip => $lb_bind_ip;
     }
   }
   if $ceilometer_api {
     cloud::loadbalancer::listen_http{
       'ceilometer_api_cluster':
         ports     => $ks_ceilometer_public_port,
-        listen_ip => $vip_public_ip;
+        listen_ip => $lb_bind_ip;
     }
   }
   if $heat_api {
     cloud::loadbalancer::listen_http{
       'heat_api_cluster':
         ports     => $ks_heat_public_port,
-        listen_ip => $vip_public_ip;
+        listen_ip => $lb_bind_ip;
     }
   }
   if $heat_cfn_api {
     cloud::loadbalancer::listen_http{
       'heat_api_cfn_cluster':
         ports     => $ks_heat_cfn_public_port,
-        listen_ip => $vip_public_ip;
+        listen_ip => $lb_bind_ip;
     }
   }
   if $heat_cloudwatch_api {
     cloud::loadbalancer::listen_http{
       'heat_api_cloudwatch_cluster':
         ports     => $ks_heat_cloudwatch_public_port,
-        listen_ip => $vip_public_ip;
+        listen_ip => $lb_bind_ip;
     }
   }
   if $horizon {
     cloud::loadbalancer::listen_http{
       'horizon_cluster':
         ports     => $horizon_port,
-        listen_ip => $vip_public_ip;
+        listen_ip => $lb_bind_ip;
     }
   }
 
   haproxy::listen { 'galera_cluster':
-    ipaddress          => $galera_ip,
+    ipaddress          => $vip_internal_ip,
     ports              => 3306,
     options            => {
       'mode'           => 'tcp',
